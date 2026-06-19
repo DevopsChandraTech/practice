9dba0b8453773ab7de876565d716a0d5da5a3c1b
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-19 16:27:31 +0530

    practice-shell

---
+++ 01.conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh How are you"
+echo "Suresh:: I am fine, How are you Ramesh"
+echo "Suresh:: what are you learning now Ramesh"
+echo "Ramesh:: I am learning shell-scripting Suresh"
---
+++ 02.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAME1=Chandu    
+NAME2=Anu
+
+echo "$NAME1:: Hi $NAME2 How are you"
+echo "$NAME2:: I am fine, How are you $NAME1"
+echo "$NAME2:: what are you learning now $NAME1"
+echo "$NAME1:: I am learning shell-scripting $NAME2"
