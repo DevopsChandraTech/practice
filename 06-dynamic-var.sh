d1dc356babb2d4dfa4d1a7e3ed50292c9b96d005
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-19 16:45:00 +0530

    practice-shell

---
+++ 06-dynamic-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+START_TIME=$(date +%s) # $() executes inside the command and gives the output
+
+sleep 10 
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo "the total time in $TOTAL_TIME Secs."
