a31cccc772bdc4fee14fb46f232ce60ddde85003
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-19 17:29:38 +0530

    practice-shell

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-read NUM
+# read NUM
 
-if (($NUM % 2 == 0)); then
-    echo "given number $NUM is even."
-else
-    echo "given number is not an even."
+# if (($NUM % 2 == 0)); then
+#     echo "given number $NUM is even."
+# else
+#     echo "given number is not an even."
+# fi
+
+# check the number is greater than 10
+
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]; then
+    echo "given number $NUMBER is greaterthan 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "given number $NUMBER is equals to 10."
+else    
+    echo "given number $NUMBER is lessthan 10."
 fi
