9e084b3922e0b417657c645caa8f6637f22bacb1
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-21 20:47:51 +0530

    shell-practice

---
+++ 15-loop-installation.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#install mysql nginx pyhton3
+#need to check user has root user privilizes or not
+#install mysql nginx python3
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "Error:: run command with root user privilizes." | tee -a $FILE_NAME
+    exit 1
+else
+    echo "Installing $2" | tee -a $FILE_NAME
+fi
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | awk -F "." '{print $1}')
+FILE_NAME="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Failure $N command not found." | tee -a $FILE_NAME
+        exit 1
+    else    
+        echo -e "$G Success $N Installing $2" | tee -a $FILE_NAME
+    fi
+}
+
+#installations
+for package in $@;
+do
+    dnf list installed $2
+    if [ $package -ne 0 ]; then
+        echo "Failure::$package not installed"
+        dnf install $package -y
+        exit 1
+
+    else    
+        echo "Success::$package already installed Skipping...!"
+    fi
+done
