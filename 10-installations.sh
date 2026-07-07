5bac9bc4c20b98d13dca6b0d64f7b8754392807b
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-07 20:42:43 +0530

    shell-practice

--- 10-installations.sh
+++ 10-installations.sh
@@ -16,6 +16,7 @@ else
     echo "Success::Installing the mysql"
 fi
 
+#mysql installation
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "Error:: MySql Installation is fail"
@@ -24,4 +25,35 @@ else
     echo "MySql Installation is Success."
 fi
 
+#nginx installation
+if [ $? -ne 0 ]; then
+    echo "Error:: Unable to find the command."
+    exit 1
+else 
+    echo "Success::Installing the nginx"
+fi
+
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo "Error:: Nginx Installation is fail"
+    exit 1
+else
+    echo "Nginx Installation is Success."
+fi
+
+#python3 installation
+if [ $? -ne 0 ]; then
+    echo "Error:: Unable to find the command."
+    exit 1
+else 
+    echo "Success::Installing the python3"
+fi
+
+dnf install python3 -y
+if [ $? -ne 0 ]; then
+    echo "Error:: Python3 Installation is fail"
+    exit 1
+else
+    echo "Python3 Installation is Success."
+fi
 
