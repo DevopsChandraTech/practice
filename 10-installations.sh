1ec54ff8e1e17e7c9beba7bd23d191a9019fdf69
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-07 20:37:37 +0530

    shell-practice

--- 10-installations.sh
+++ 10-installations.sh
@@ -5,11 +5,13 @@ USERID=$(id -u)
 # checking root user access
 if [ $USERID -ne 0 ]; then
     echo "Error:: Run command with root user privilizes."
+    exit 1
 fi
 
 # checking command is correct or not
 if [ $? -ne 0 ]; then
     echo "Error:: Unable to find the command."
+    exit 1
 else 
     echo "Success::Installing the mysql"
 fi
@@ -17,6 +19,7 @@ fi
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "Error:: MySql Installation is fail"
+    exit 1
 else
     echo "MySql Installation is Success."
 fi
