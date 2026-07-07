94154136209d50a9ff16a8fa751b1ec00974d46c
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-07 19:18:46 +0530

    shell-practice

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -5,6 +5,10 @@ NUMBER2=200
 
 NAME="CHANDU"
 
+FAMILY="{"Amma" "Chandu" "Anu" "Gnaipka" "Hansith"}"
+
 SUM=$(($NUMBER1 + $NUMBER2 + $NAME))
 
 echo "the sum is : $SUM"
+echo "head of the family is : ${FAMILY[0]}"
+echo "eledest of the family is : ${FAMILY[4]}"
