1b3d9e3233a589cba8b9822f53f126e3725e29cf
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-07 18:37:20 +0530

    shell-practice

--- 05-special-var.sh
+++ 05-special-var.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+echo "all arguments passed into script : $@"
+echo "all arguments passed into script : $*"
+echo "how many arguments passed into script : $#"
+echo "who is running the Script : $USER"
+echo "current working directroy of script : $PWD"
+echo "home folder of current script : $HOME"
+echo "PID of current script : $?"
+
+sleep 10
+echo "PID of last command of current script : $$"
