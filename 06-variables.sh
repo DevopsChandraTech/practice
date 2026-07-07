dc697bd321122f183afa7cd56822d124a67a4403
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-07 18:31:55 +0530

    shell-practice

---
+++ 06-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2 How are you"
+echo "$PERSON2:: I am fine How are you $PERSON1"
+echo "$PERSON1:: I am also what are you doing now $PERSON2"
+echo "$PERSON2:: I am coocking for my family"
+
