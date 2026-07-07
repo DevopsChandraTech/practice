9a0682c876b693a50836fa718ed788d31baffc48
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-07 18:39:46 +0530

    shell-practice

--- 05-special-var.sh
+++ 05-special-var.sh
@@ -8,5 +8,5 @@ echo "current working directroy of script : $PWD"
 echo "home folder of current script : $HOME"
 echo "PID of current script : $?"
 
-sleep 10
+sleep 10&
 echo "PID of last command of current script : $$"
