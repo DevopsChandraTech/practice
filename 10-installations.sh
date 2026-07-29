47f46c80313fd120b832a218d32ab41b2f052705
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-29 12:44:41 +0530

    shell-practice

--- 10-installations.sh
+++ 10-installations.sh
@@ -2,6 +2,11 @@
 
 echo "the script started executed in $(date)"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
@@ -15,7 +20,7 @@ if [ $? -ne 0 ]; then
     echo "Error:: MySql Command Not found"
     exit 1
 else
-    echo "Installing MySql Success..!"
+    echo "Installing MySql $G Success..! $N"
 fi
 
 #nginx installation
@@ -24,7 +29,7 @@ if [ $? -ne 0 ]; then
     echo "Error:: Nginx Command Not found"
     exit 1
 else
-    echo "Installing Nginx Success..!"
+    echo "Installing Nginx $G Success..! $N"
 fi
 
 #unzip installation
@@ -33,6 +38,6 @@ if [ $? -ne 0 ]; then
     echo "Error:: Unzip Command Not found"
     exit 1
 else
-    echo "Installing Unzip Success..!"
+    echo "Installing Unzip $G Success..! $N"
 fi
 
