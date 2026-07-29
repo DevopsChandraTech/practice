a0e133874c0459683c65aba3d807e43d0dbb5659
DevOpsChandraTech <devopschandra42@gmail.com>
2026-07-29 12:36:17 +0530

    shell-practice

--- 10-installations.sh
+++ 10-installations.sh
@@ -6,11 +6,24 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Error:: run script with root user privilizes"
+    exit 1
 fi
 
+#mysql installation
 dnf install mysql -y
 if [ $? -ne 0 ]; then
-    echo "Error:: Command Not found"
+    echo "Error:: MySql Command Not found"
+    exit 1
 else
     echo "Installing MySql Success..!"
 fi
+
+#nginx installation
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo "Error:: Nginx Command Not found"
+    exit 1
+else
+    echo "Installing Nginx Success..!"
+fi
+
