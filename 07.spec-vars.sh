7d30345d65b99d2390a1391e170a20fbad91cec9
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-17 20:23:37 +0530

    shell-practice

--- 07.spec-vars.sh
+++ 07.spec-vars.sh
@@ -2,8 +2,9 @@
 
 echo "all arguments run into the script : $@"
 echo "all arguments run into the script : $*"
+echo "current script name is : $0"
 echo "home directory of the script : $HOME"
 echo "current user of the script : $USER"
 echo "currect working directory of the script : $PWD"
-echo "PID of the script : $?"
+echo "PID of the script : $$"
 echo "PID of the last command : $!"
