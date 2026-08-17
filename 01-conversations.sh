5f97a85a706f81ddb5e09611a9681729e8582ac6
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-17 19:25:02 +0530

    shell-practice

---
+++ 01-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: How are you Suresh"
+echo "Suresh:: fine Ramesh How are you"
+echo "Ramesh:: Fine What are you doing now?"
+echo "Suresh:: I am learning Shell Scripting Ramesh"
---
+++ 02.conversations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Chandu  
+PERSON2=Anu
+
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: fine $PERSON1 How are you"
+echo "$PERSON1:: Fine What are you doing now?"
+echo "$PERSON2:: I am learning Shell Scripting $PERSON1"
