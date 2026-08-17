504d4824805deed7728aa3bb7db9b47eb286481f
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-17 20:25:33 +0530

    shell-practice

--- 07.spec-vars.sh
+++ 07.spec-vars.sh
@@ -2,6 +2,7 @@
 
 echo "all arguments run into the script : $@"
 echo "all arguments run into the script : $*"
+echo "how many arguments passed into script : $#"
 echo "current script name is : $0"
 echo "home directory of the script : $HOME"
 echo "current user of the script : $USER"
