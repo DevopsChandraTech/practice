6d40a58af01d6eccf757943cf8a84066127c5d37
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-18 12:28:46 +0530

    shell-practice

---
+++ 08-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# check the number divided by even or not
+
+echo "enter the number"
+
+read "NUMBER"
+
+if [ ($NUMBER / 2) = 0 ]; then
+    echo "Given Number $NUMBER is Even."
+else 
+    echo "Given Number $NUMBER is Not Even."
+fi
