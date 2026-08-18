3e3c2da540dd0c8509ed951a01c4e563fd5f62c4
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-18 12:45:46 +0530

    shell-practice

---
+++ 09-installations.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
+    echo "Error:: run command with root user privilizes"
+fi
+
+#Installing MySql
+dnf install mysql -y
+if [ $? -ne 0 ];
+    echo "Error:: command not found"
+else 
+    echo "Success:: Installing MySql"
+fi
+
+#Installing Nginx
+dnf install nginx -y
+if [ $? -ne 0 ];
+    echo "Error:: command not found"
+else 
+    echo "Success:: Installing MySql"
+fi
+
+#Installing Unzip
+dnf install unzip -y
+if [ $? -ne 0 ];
+    echo "Error:: command not found"
+else 
+    echo "Success:: Installing MySql"
+fi
