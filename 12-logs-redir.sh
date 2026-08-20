68c39d0210407f63473a0c7dd311131e26f3dcd9
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-20 11:18:58 +0530

    shell-practice

--- 12-logs-redir.sh
+++ 12-logs-redir.sh
@@ -9,7 +9,7 @@ echo "the script started and executed at $(date)"
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
-    echo "Error:: run user with root user privilizes" | tee -a &>> $LOG_FILE
+    echo "Error:: run user with root user privilizes" | tee -a $LOG_FILE
     exit 1
 fi
 
@@ -21,10 +21,10 @@ mkdir -p $LOG_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 is $R Failure..! $N"  | tee -a &>> $LOG_FILE
+        echo -e "Installing $2 is $R Failure..! $N"  | tee -a $LOG_FILE
         exit 1
     else 
-        echo -e "Installing $2 is $G Success..! $N" | tee -a &>> $LOG_FILE
+        echo -e "Installing $2 is $G Success..! $N" | tee -a $LOG_FILE
     fi
 }
 
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y &>> $LOG_FILE
     VALIDATE $? MySql
 else
-    echo -e "MySql already Installed $Y Skipping..! $N" | tee -a &>> $LOG_FILE
+    echo -e "MySql already Installed $Y Skipping..! $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed nginx &>> $LOG_FILE
@@ -41,7 +41,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y &>> $LOG_FILE
     VALIDATE $? Nginx
 else
-    echo -e "Nginx already Installed $Y Skipping..! $N" | tee -a &>> $LOG_FILE
+    echo -e "Nginx already Installed $Y Skipping..! $N" | tee -a $LOG_FILE
 fi
 
 dnf list installed unzip &>> $LOG_FILE
@@ -49,6 +49,6 @@ if [ $? -ne 0 ]; then
     dnf install unzip -y &>> $LOG_FILE
     VALIDATE $? Unzip
 else
-    echo -e "Unzip already Installed $Y Skipping..! $N" | tee -a &>> $LOG_FILE
+    echo -e "Unzip already Installed $Y Skipping..! $N" | tee -a $LOG_FILE
 fi
 
