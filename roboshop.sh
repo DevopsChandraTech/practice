e7a839a36a1e8cb8537cacfd7dbf91d388c7c8d1
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-21 19:45:28 +0530

    shell-practice

--- roboshop.sh
+++ roboshop.sh
@@ -36,16 +36,16 @@ VALIDATE(){
 
 for instance in $@;
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --query 'Instances[0].InstanceId' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --query 'Instances[0].InstanceId' --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
-        --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
-        --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
+        --query "Reservations[0].Instances[0].PublicIpAddress" --output text) 
     fi
 
     $instance=$IP
