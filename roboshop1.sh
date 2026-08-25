b7233ac067dd53bc8c665d1d57eaa6c7cb06f72a
DevOpsChandraTech <devopschandra42@gmail.com>
2026-08-25 11:10:17 +0530

    shell-practice

--- roboshop1.sh
+++ roboshop1.sh
@@ -8,7 +8,7 @@ START_TIME=$(date +%s)
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --query 'Instances[0].InstanceId' --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances \
